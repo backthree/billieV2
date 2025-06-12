f0bd5f466684bcf3d220145fd1502a339b778b31
5almiakki <jpui9635@gmail.com>
2025-06-13 01:54:10 +0900

    Refactor: 회원 리포지토리 리펙터링 #10
    - 순수 리포지토리와 JPA 리포지토리 인터페이스 분리
    - 순수 리포지토리 구현체의 멤버로 JPA 리포지토리 추가

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/member/infrastructure/persistence/jpa/MemberJpaRepository.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/member/infrastructure/persistence/jpa/MemberJpaRepository.java
@@ -1,15 +1,11 @@
-package com.nextdoor.nextdoor.domain.member.repository;
+package com.nextdoor.nextdoor.domain.member.infrastructure.persistence.jpa;
 
 import com.nextdoor.nextdoor.domain.member.domain.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface MemberRepository {
-
-    Member save(Member member);
-
-    Optional<Member> findById(Long id);
+public interface MemberJpaRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByUserKey(String userKey);
 }
