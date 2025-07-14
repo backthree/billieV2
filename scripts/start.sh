9a86ee8ad5110e573bf3ddde0cb8bdcc56706c9a
Choijake <shameless8@naver.com>
2025-07-14 16:27:00 +0900

    ci: 환경변수 불러오기 추가

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 cd /home/ubuntu/app
 
+SPRING_DATASOURCE_URL=$(aws ssm get-parameter --name "/billie/prod/SPRING_DATASOURCE_URL" --with-decryption --query "Parameter.Value" --output text)
+SPRING_DATASOURCE_USERNAME=$(aws ssm get-parameter --name "/billie/prod/SPRING_DATASOURCE_USERNAME" --with-decryption --query "Parameter.Value" --output text)
+SPRING_DATASOURCE_PASSWORD=$(aws ssm get-parameter --name "/billie/prod/SPRING_DATASOURCE_PASSWORD" --with-decryption --query "Parameter.Value" --output text)
+SPRING_DATA_MONGODB_URI=$(aws ssm get-parameter --name "/billie/prod/SPRING_DATA_MONGODB_URI" --with-decryption --query "Parameter.Value" --output text)
+SPRING_DATA_REDIS_HOST=$(aws ssm get-parameter --name "/billie/prod/SPRING_DATA_REDIS_HOST" --with-decryption --query "Parameter.Value" --output text)
+SPRING_ELASTICSEARCH_URIS=$(aws ssm get-parameter --name "/billie/prod/SPRING_ELASTICSEARCH_URIS" --with-decryption --query "Parameter.Value" --output text)
+
 cat > .env << EOF
 SPRING_DATASOURCE_URL=${SPRING_DATASOURCE_URL}
 SPRING_DATASOURCE_USERNAME=${SPRING_DATASOURCE_USERNAME}
