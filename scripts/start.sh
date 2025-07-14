8a43e415b9b95b3957908cba803059793c9e869f
Choijake <shameless8@naver.com>
2025-07-14 21:02:18 +0900

    ci: 환경변수 변수명 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,10 +9,10 @@ SPRING_DATA_REDIS_HOST=$(aws ssm get-parameter --name "/billie/prod/SPRING_DATA_
 SPRING_ELASTICSEARCH_URIS=$(aws ssm get-parameter --name "/billie/prod/SPRING_ELASTICSEARCH_URIS" --with-decryption --query "Parameter.Value" --output text)
 SPRING_RABBITMQ_USERNAME=$(aws ssm get-parameter --name "/billie/prod/SPRING_RABBITMQ_USERNAME" --with-decryption --query "Parameter.Value" --output text)
 SPRING_RABBITMQ_PASSWORD=$(aws ssm get-parameter --name "/billie/prod/SPRING_RABBITMQ_PASSWORD" --with-decryption --query "Parameter.Value" --output text)
-KAKAO_CLIENT_ID=$(aws ssm get-parameter --name "/billie/prod/KAKAO_CLIENT_ID" --with-decryption --query "Parameter.Value" --output text)
-KAKAO_CLIENT_SECRET=$(aws ssm get-parameter --name "/billie/prod/KAKAO_CLIENT_SECRET" --with-decryption --query "Parameter.Value" --output text)
-GEMINI_PROJECT_ID=$(aws ssm get-parameter --name "/billie/prod/GEMINI_PROJECT_ID" --with-decryption --query "Parameter.Value" --output text)
-FINTECH_API_KEY=$(aws ssm get-parameter --name "/billie/prod/FINTECH_API_KEY" --with-decryption --query "Parameter.Value" --output text)
+KAKAO_CLIENT_ID=$(aws ssm get-parameter --name "/billie/prod/SPRING_SECURITY_OAUTH2_CLIENT_REGISTRATION_KAKAO_CLIENT" --with-decryption --query "Parameter.Value" --output text)
+KAKAO_CLIENT_SECRET=$(aws ssm get-parameter --name "/billie/prod/SPRING_SECURITY_OAUTH2_CLIENT_REGISTRATION_KAKAO_CLIENT-SECRET" --with-decryption --query "Parameter.Value" --output text)
+GEMINI_PROJECT_ID=$(aws ssm get-parameter --name "/billie/prod/CUSTOM_GOOGLE_AI_GEMINI_PROJECT-ID" --with-decryption --query "Parameter.Value" --output text)
+FINTECH_API_KEY=$(aws ssm get-parameter --name "/billie/prod/CUSTOM_FINTECH_APIKEY" --with-decryption --query "Parameter.Value" --output text)
 
 
 cat > .env << EOF
@@ -24,10 +24,10 @@ SPRING_DATA_REDIS_HOST=${SPRING_DATA_REDIS_HOST}
 SPRING_ELASTICSEARCH_URIS=${SPRING_ELASTICSEARCH_URIS}
 SPRING_RABBITMQ_USERNAME=${SPRING_RABBITMQ_USERNAME}
 SPRING_RABBITMQ_PASSWORD=${SPRING_RABBITMQ_PASSWORD}
-SPRING_SECURITY_OAUTH2_CLIENT_REGISTRATION_KAKAO_CLIENT-ID=${KAKAO_CLIENT_ID}
-SPRING_SECURITY_OAUTH2_CLIENT_REGISTRATION_KAKAO_CLIENT-SECRET=${KAKAO_CLIENT_SECRET}
-CUSTOM_GOOGLE_AI_GEMINI_PROJECT-ID=${GEMINI_PROJECT_ID}
-CUSTOM_FINTECH_APIKEY=${FINTECH_API_KEY}
+SPRING_SECURITY_OAUTH2_CLIENT_REGISTRATION_KAKAO_CLIENT-ID=${SPRING_SECURITY_OAUTH2_CLIENT_REGISTRATION_KAKAO_CLIENT}
+SPRING_SECURITY_OAUTH2_CLIENT_REGISTRATION_KAKAO_CLIENT-SECRET=${SPRING_SECURITY_OAUTH2_CLIENT_REGISTRATION_KAKAO_CLIENT}
+CUSTOM_GOOGLE_AI_GEMINI_PROJECT-ID=${CUSTOM_GOOGLE_AI_GEMINI_PROJECT}
+CUSTOM_FINTECH_APIKEY=${CUSTOM_FINTECH_APIKEY}
 EOF
 
 docker pull choijake/billie-app:latest
