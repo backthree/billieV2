4a2743e22f75680a7e0221aa7a2398afe2db5881
Choijake <shameless8@naver.com>
2025-07-24 16:03:56 +0900

    feat: 커서 기반 조회 쿼리 추가 - PostRepository

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/post/repository/PostRepository.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/post/repository/PostRepository.java
@@ -1,9 +1,20 @@
 package com.nextdoor.nextdoor.domain.post.repository;
 
 import com.nextdoor.nextdoor.domain.post.domain.Post;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @Query("SELECT p FROM Post p WHERE p.id > :lastId ORDER BY p.id ASC")
+    List<Post> findPostsAfter(
+            @Param("lastId") Long lastId,
+            Pageable pageable
+    );
 }
