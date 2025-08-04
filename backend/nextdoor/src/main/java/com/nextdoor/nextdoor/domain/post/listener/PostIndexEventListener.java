718475212e89cf967b93397424181682545c9c6d
Choijake <shameless8@naver.com>
2025-08-04 15:44:56 +0900

    feat: sqs 메시지 리스너 구현

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/post/listener/PostIndexEventListener.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/post/listener/PostIndexEventListener.java
@@ -2,28 +2,30 @@ package com.nextdoor.nextdoor.domain.post.listener;
 
 import com.nextdoor.nextdoor.domain.post.event.PostCreatedEvent;
 import com.nextdoor.nextdoor.domain.post.event.PostUpdatedEvent;
-import com.nextdoor.nextdoor.domain.post.search.PostIndexService;
+import com.nextdoor.nextdoor.domain.post.search.messaging.producer.PostIndexingProducer;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
+@Profile("api")
 @RequiredArgsConstructor
 public class PostIndexEventListener {
 
-    private final PostIndexService postIndexService;
+    private final PostIndexingProducer postIndexingProducer;
 
     @Async("asyncExecutor")
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    public void onPostCreated(PostCreatedEvent postCreatedEvent) {
-        postIndexService.indexSinglePost(postCreatedEvent.getPostId());
+    public void onPostCreated(PostCreatedEvent event) {
+        postIndexingProducer.requestSinglePostIndexing(event.getPostId());
     }
 
     @Async("asyncExecutor")
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    public void onPostDeleted(PostUpdatedEvent postUpdatedEvent) {
-        postIndexService.deleteSingleIndex(postUpdatedEvent.getPostId());
+    public void onPostDeleted(PostUpdatedEvent event) {
+        postIndexingProducer.requestSinglePostDeletion(event.getPostId());
     }
 }
