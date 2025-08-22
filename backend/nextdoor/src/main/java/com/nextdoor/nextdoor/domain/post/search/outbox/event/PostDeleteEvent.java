cfe08a5b2d269b1793b08f985bbacd03f12f4d81
Choijake <shameless8@naver.com>
2025-08-22 15:44:34 +0900

    feat: PostIndexEvent 구현체에 기본 생성자 추가, final 필드를 위해 force = true 사용

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/post/search/outbox/event/PostDeleteEvent.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/post/search/outbox/event/PostDeleteEvent.java
@@ -1,10 +1,14 @@
 package com.nextdoor.nextdoor.domain.post.search.outbox.event;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor(force = true)
+@AllArgsConstructor
 public class PostDeleteEvent implements PostIndexEvent {
     private final String type = "DELETE";
     private Long postId;
--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/post/search/outbox/event/PostUpsertEvent.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/domain/post/search/outbox/event/PostUpsertEvent.java
@@ -1,10 +1,14 @@
 package com.nextdoor.nextdoor.domain.post.search.outbox.event;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor(force = true)
+@AllArgsConstructor
 public class PostUpsertEvent implements PostIndexEvent {
     private final String type = "UPSERT";
     private Long postId;
