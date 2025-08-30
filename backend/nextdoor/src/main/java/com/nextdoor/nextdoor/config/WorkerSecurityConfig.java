b63792f440257e07ecb95cebe1caad2d8b2b770f
Choijake <shameless8@naver.com>
2025-08-30 18:01:16 +0900

    chore: outbox 시큐리티 설정 삭제

--- backend/nextdoor/src/main/java/com/nextdoor/nextdoor/config/WorkerSecurityConfig.java
+++ backend/nextdoor/src/main/java/com/nextdoor/nextdoor/config/WorkerSecurityConfig.java
@@ -12,7 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@Profile({"worker", "outbox"})
+@Profile("worker")
 public class WorkerSecurityConfig {
 
     @Bean
